6dec6fd32fd219f1b5cfdc6987d8c215cf22df6d
Debora <you@example.com>
2024-10-24 15:50:33 +0200

    test

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 
-app.get('/', (req, res) => {
-	res.send('Bienvenue sur notre API REST simple!');
-});
+app.get("/", (req, res) => {
+    res.json({
+        msg: "hello from API"
+    })
+})
 
 app.listen(port, () => {
 	console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
