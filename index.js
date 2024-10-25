cb10f80411848b9fa3c0e009a2eee936da1ea82d
Debora <you@example.com>
2024-10-25 09:06:46 +0200

    test

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const port = 3000;
 // Middleware pour traiter les données JSON envoyées dans le body
 app.use(express.json());
 
+const users = [
+	{ id: 1, firstName: "John", lastName: "Doe", role: "admin" },
+	{ id: 2, firstName: "Jane", lastName: "Smith", role: "user" },
+	{ id: 3, firstName: "Alice", lastName: "Johnson", role: "moderator" },
+	{ id: 4, firstName: "Bob", lastName: "Brown", role: "user" },
+	{ id: 5, firstName: "Charlie", lastName: "Davis", role: "admin" },
+]
+
+
 // POST : CRÉER un nouvel utilisateur, basé sur les données passées dans le corps(body) de la requête
 app.post("/", (req, res) => {
 	// récupérer toutes les données qui arrivent dans le corps de la requête (body)
