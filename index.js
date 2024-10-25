5909149ee8caa50089e867b4f0293f0020c9a6b8
Debora <you@example.com>
2024-10-25 11:18:01 +0200

    test

--- index.js
+++ index.js
@@ -19,3 +19,5 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
 	console.log(`Serveur en cours d'exécution sur http://localhost:${port}/`);
 });
+
+
--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,16 @@ const usersArray = [
 	{ id: 5, firstName: "Charlie", lastName: "Davis", role: "admin" },
 
 ]
+
+const users = [
+	{ id: 1, firstName: "John", lastName: "Doe", role: "admin" },
+	{ id: 2, firstName: "Jane", lastName: "Smith", role: "user" },
+	{ id: 3, firstName: "Alice", lastName: "Johnson", role: "moderator" },
+	{ id: 4, firstName: "Bob", lastName: "Brown", role: "user" },
+	{ id: 5, firstName: "Charlie", lastName: "Davis", role: "admin" },
+];
+
+
 // GET METHOD
 router.get("/users", (req, res) => {
     res.json(usersArray)
