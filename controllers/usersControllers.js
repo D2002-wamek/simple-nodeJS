ea477232e510d4dad82f9643674f0f91067762a7
Debora <you@example.com>
2024-10-27 01:07:47 +0200

    test

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,31 +1,39 @@
-const db = require("../database")
+const db = require("../database");
 
+// Fonction pour insérer un nouvel utilisateur dans la base de données
 exports.getAllUsers = function (req, res) {
-    db.run(
+	const { firstName, lastName } = req.body;  // Extraire les valeurs depuis le corps de la requête
+
+	db.run(
 		"INSERT INTO users (firstName, lastName) VALUES (?, ?)",
 		[firstName, lastName],
 		function (err) {
 			if (err) {
-				res.status(500).json({ error: err.message })
+				res.status(500).json({ error: err.message });
 			} else {
-				res.status(201).json({ id: this.lastID, firstName })
+				res.status(201).json({ id: this.lastID, firstName, lastName });
 			}
 		}
-	)
+	);
+};
 
-}
-
+// Fonction pour récupérer tous les utilisateurs de la base de données
 exports.createNewUser = (req, res) => {
-    const { firstName, lastName } = req.body;
-	const lastId = users[users.length - 1].id;
-	const newId = lastId + 1;
+	const { firstName, lastName } = req.body;
 
-	const newUser = {
-		id: newId,
-		firstName,
-		lastName
-	};
-
-	users.push(newUser);
-	res.status(201).json(newUser);
-}
+	db.run(
+		"INSERT INTO users (firstName, lastName) VALUES (?, ?)",
+		[firstName, lastName],
+		function (err) {
+			if (err) {
+				res.status(500).json({ error: err.message });
+			} else {
+				res.status(201).json({
+					id: this.lastID,
+					firstName,
+					lastName
+				});
+			}
+		}
+	);
+};
