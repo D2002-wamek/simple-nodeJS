99d8abff419ea0ade23b0dbe467511aa69319f85
Debora <you@example.com>
2024-10-27 02:35:58 +0100

    test

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -2,6 +2,47 @@ const db = require("../database");
 
 // Fonction pour insérer un nouvel utilisateur dans la base de données
 exports.getAllUsers = function (req, res) {
+
+    const { firstName, lastName } = req.body;  // Extraire les valeurs depuis le corps de la requête
+
+    // regex pour alphanumérique seulement
+	function isAlphanumeric(str) {
+		const regex = /^[a-zA-Z0-9]+$/
+		return regex.test(str)
+	}
+
+
+
+    if (!firstName || !lastName) {
+        return res.status(400).json({error: "the first name and the name are required ! "})
+    }
+    if (typeof firstName !== "string") {
+        return res.status(400).json({error: "that's a weird name ! "})
+    }
+    if (!isAlphanumeric(firstName)){
+		return res.status(400).json({ error: "Ce nom n'est pas autorisé !" })
+
+    }
+
+
+	db.run(
+		"INSERT INTO users (firstName, lastName) VALUES (?, ?)",
+		[firstName, lastName],
+		function (err) {
+			if (err) {
+				res.status(500).json({ error: err.message });
+			} else {
+				res.status(201).json({ id: this.lastID, firstName, lastName });
+			}
+		}
+	);
+
+    
+};
+
+// Fonction pour récupérer tous les utilisateurs de la base de données
+exports.createNewUser = (req, res) => {
+
     const { firstName, lastName } = req.body;
 
 	db.run(
@@ -16,32 +57,6 @@ exports.getAllUsers = function (req, res) {
 					firstName,
 					lastName
 				});
-			}
-		}
-	);
-	
-};
-
-// Fonction pour récupérer tous les utilisateurs de la base de données
-exports.createNewUser = (req, res) => {
-
-    const { firstName, lastName } = req.body;  // Extraire les valeurs depuis le corps de la requête
-
-    if (!firstName) {
-        return res.status(400).json({error: "the first name is required ! "})
-    }
-    if (typeof firstName !== "string") {
-        return res.status(400).json({error: "that's a weird name ! "})
-    }
-
-	db.run(
-		"INSERT INTO users (firstName, lastName) VALUES (?, ?)",
-		[firstName, lastName],
-		function (err) {
-			if (err) {
-				res.status(500).json({ error: err.message });
-			} else {
-				res.status(201).json({ id: this.lastID, firstName, lastName });
 			}
 		}
 	);
