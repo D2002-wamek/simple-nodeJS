b724b79f0d2adebbe780317e25b7e5093714a6d9
Debora <you@example.com>
2024-10-27 02:57:46 +0200

    test

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -2,7 +2,30 @@ const db = require("../database");
 
 // Fonction pour insérer un nouvel utilisateur dans la base de données
 exports.getAllUsers = function (req, res) {
-	const { firstName, lastName } = req.body;  // Extraire les valeurs depuis le corps de la requête
+    const { firstName, lastName } = req.body;
+
+	db.run(
+		"INSERT INTO users (firstName, lastName) VALUES (?, ?)",
+		[firstName, lastName],
+		function (err) {
+			if (err) {
+				res.status(500).json({ error: err.message });
+			} else {
+				res.status(201).json({
+					id: this.lastID,
+					firstName,
+					lastName
+				});
+			}
+		}
+	);
+	
+};
+
+// Fonction pour récupérer tous les utilisateurs de la base de données
+exports.createNewUser = (req, res) => {
+
+    const { firstName, lastName } = req.body;  // Extraire les valeurs depuis le corps de la requête
 
     if (!firstName) {
         return res.status(400).json({error: "the first name is required ! "})
@@ -22,27 +45,5 @@ exports.getAllUsers = function (req, res) {
 			}
 		}
 	);
-};
-
-// Fonction pour récupérer tous les utilisateurs de la base de données
-exports.createNewUser = (req, res) => {
-	const { firstName, lastName } = req.body;
-
-    
-
-	db.run(
-		"INSERT INTO users (firstName, lastName) VALUES (?, ?)",
-		[firstName, lastName],
-		function (err) {
-			if (err) {
-				res.status(500).json({ error: err.message });
-			} else {
-				res.status(201).json({
-					id: this.lastID,
-					firstName,
-					lastName
-				});
-			}
-		}
-	);
+	
 };
