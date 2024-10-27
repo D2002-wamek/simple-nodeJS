adf837d92a11874f7eaa1e6b48d31a0914cb7727
Debora <you@example.com>
2024-10-27 02:53:34 +0200

    test

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -4,6 +4,9 @@ const db = require("../database");
 exports.getAllUsers = function (req, res) {
 	const { firstName, lastName } = req.body;  // Extraire les valeurs depuis le corps de la requête
 
+    if (!firstName) {
+        return res.status(400).json({error: "the first name is required ! "})
+    }
     if (typeof firstName !== "string") {
         return res.status(400).json({error: "that's a weird name ! "})
     }
